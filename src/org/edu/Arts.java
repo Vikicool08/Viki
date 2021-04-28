0362ec3092a3c3d74486d70d9842596bd3da4b08
Viki <vikicool08@gmail.com>
2021-04-28 19:13:30 +0530

    new project

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,31 @@
+package org.edu;
+
+public class Arts extends Education {
+	public void bSc() {
+	System.out.println("50%");
+}
+	public void bEd() {
+		System.out.println("60%");
+	}
+	public void bA() {
+		System.out.println("65%");
+	}
+	public void bBA() {
+		System.out.println("70%");
+	}
+	public void ug() {
+		System.out.println("45%");
+	}
+	public void pg() {
+		System.out.println("55%");
+	}
+	public static void main(String[] args) {
+		Arts a = new Arts();
+		a.bA();
+		a.bBA();
+		a.bEd();
+		a.bSc();
+		a.ug();
+		a.pg();
+	}
+}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,10 @@
+package org.edu;
+
+public class Education {
+public void ug() {
+	System.out.println("80%");
+}
+public void pg() {
+	System.out.println("90%");
+}
+}
